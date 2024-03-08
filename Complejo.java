3c251db14a3a055fcf9d33e3d69710127bb936b8
gabriel-1302 <gabriel.velasquez.1290@gmail.com>
2024-03-08 02:54:58 -0400

    Gabriel_Velasquez creacion de clase

---
+++ Complejo.java
@@ -0,0 +1,18 @@
+import java.lang.Math;
+import java.util.Scanner;
+
+public class Complejo {
+    private double real;
+    private double imaginario;
+
+    // Constructor
+    public Complejo(double real, double imaginario) {
+        this.real = real;
+        this.imaginario = imaginario;
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+}
